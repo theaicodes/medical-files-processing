3467d24a6aaee654100fc6033912241ef3659b07
40uf411 <40uf4l1@gmail.com>
2022-01-29 23:53:31 +0100

    final'

--- 3d viz.py
+++ 3d viz.py
@@ -1,25 +1,24 @@
-import matplotlib
-
 from matplotlib import pylab as plt
 import nibabel as nib
 from nibabel import nifti1
 from nibabel.viewers import OrthoSlicer3D
 
-img = nib.load("./scan.nii.gz")
-print(img)
-print(img.header['db_name']) # output header information
+# print the header of the file
+image = nib.load('./scan.nii.gz')
+mask = nib.load('./mask.nii.gz')
+print("Image shape: ", image.shape)
+print("Mask shape: ", mask.shape)
+print('\n', "#" * 10, " Image header ", "#" * 10, '\n')
+print(image.header) # output header information
+print('\n', "#" * 10, " Mask header ", "#" * 10, '\n')
+print(mask.header) # output header information
 
-#shape has four parameters patient001_4d.nii.gz
-#shape has three parameters patient001_frame01.nii.gz patient001_frame12.nii.gz
-#shape has three parameters patient001_frame01_gt.nii.gz patient001_frame12_gt.nii.gz
-width, height, queue = img.dataobj.shape
-OrthoSlicer3D(img.dataobj).show()
+#shape has four parameters scan.nii.gz
+width, height, queue = image.dataobj.shape
+OrthoSlicer3D(image.dataobj).show()
 
-num = 1
-for i in range(0, queue, 10):
-    img_arr = img.dataobj[:, :, i]
+for num, i in enumerate(range(0, queue, 10), start=1):
+    img_arr = image.dataobj[:, :, i]
     plt.subplot(5, 5, num)
     plt.imshow(img_arr, cmap='gray')
-    num += 1
-
 plt.show()
